54829ebebb285bab091baa29d3b4d293bcc4041d
hissa <hissamssa7@gmail.com>
2017-07-28 01:19:40 +0900

    コマンドライン引数で初期値を設定できるように変更

--- ApplicationNameGenerator.cs
+++ ApplicationNameGenerator.cs
@@ -2,6 +2,11 @@ using System;
 class ProcessNameTest{
     static void Main(string[] args){
         string input = "";
+        if(args.Length > 0){
+            input = args[0];
+            Console.Title = input;
+            Console.WriteLine("コマンドライン引数に渡された{0}が設定されました。", input);
+        }
         Console.WriteLine("入力した文字列をタイトルに設定します。");
         Console.WriteLine("何も入力せずにEnterを入力すると終了します。");
         do{
